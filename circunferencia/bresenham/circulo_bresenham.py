001a0e2c4fb02d364c52a6c60f352e2e04dd9c1e
GuiBatalhoti <guibatalhoti@gmail.com>
2022-11-09 16:19:35 -0300

    Separando interface_desenho do circulo bresenham

--- circunferencia/bresenham/circulo_bresenham.py
+++ circunferencia/bresenham/circulo_bresenham.py
@@ -5,7 +5,7 @@ import numpy as np
 from math import cos, sin, sqrt, pow
 
 
-def draw_circle(window, color, posStart, posNow):
+def circulo_bresenham(window, color, posStart, posNow):
     cx, cy = posStart
     x, y = posNow
     radius = sqrt(pow((x - cx), 2) + pow((y - cy), 2))
@@ -33,33 +33,3 @@ def draw_symmetric(window, color, x, y, cx, cy):
     pygame.draw.line(window, color, ((cx + y), (cy - x)), ((cx + y)+1, (cy - x)+1))
     pygame.draw.line(window, color, ((cx - y), (cy + x)), ((cx - y)+1, (cy + x)+1))
     pygame.draw.line(window, color, ((cx - y), (cy - x)), ((cx - y)+1, (cy - x)+1))
-
-
-
-window = pygame.display.set_mode((1200, 600))
-points = []
-begin = False
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            
-    window.fill((255, 255, 255))
-
-    if any(pygame.mouse.get_pressed()) and not begin:
-        posStart = pygame.mouse.get_pos()
-        begin = True
-
-    if begin:
-        posNow = pygame.mouse.get_pos()
-        draw_circle(window, (255,0,0), posStart, posNow)
-
-    if not any(pygame.mouse.get_pressed()) and begin:
-        points.append((posStart, posNow))
-        begin = False
-
-    for i in range(len(points)):
-        draw_circle(window, (0,0,0), points[i][0], points[i][1])                                                                              
-
-    pygame.display.flip()

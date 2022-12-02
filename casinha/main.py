c827e5a00f7395e576dfe399967d5c0d833005c5
GuiBatalhoti <guibatalhoti@gmail.com>
2022-12-02 00:32:22 -0300

    oepração de rotação no centro do objeto finaliza

--- casinha/main.py
+++ casinha/main.py
@@ -1,8 +1,8 @@
-# Os pontos da casa são organizados da seguinte maneira:
+# Os pontos_modificados da casa são organizados da seguinte maneira:
 # A = (0,0,0), B = (100,0,0), C = (100,0,100), D = (0,0,100),
 # E = (0,100,0), F = (100,100,0), G = (100,100,100), H = (0,100,100),
 # I = (50,150,0), J = (50,150,100).
-# Os pontos que se ligam são 
+# Os pontos_modificados que se ligam são 
 # - AB, AD, AE;
 # - BC, BF;
 # - CD, CG;
@@ -18,7 +18,6 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPixmap, QColor, QPainter, QPen
 import sys
 import numpy as np
-from copy import deepcopy
 
 
 class UI(QWidget):
@@ -43,12 +42,21 @@ class UI(QWidget):
         self.btn_resetar_pontos = self.findChild(QPushButton, "btnResetarPontos")
 
         #Pontos originais da casinha
-        self.pontos_originais = {"a":[0,0,0,1], "b":[100,0,0,1], "c":[100,0,100,1], "d":[0,0,100,1], 
-                                 "e":[0,100,0,1], "f":[100,100,0,1], "g":[100,100,100,1], "h":[0,100,100,1],
-                                 "i":[50,150,0,1], "j":[50,150,100,1]}
+        self.pontos_originais = {"a":[0.0,0.0,0.0,1.0], "b":[100.0,0.0,0.0,1.0], "c":[100.0,0.0,100.0,1.0], "d":[0.0,0.0,100.0,1.0], 
+                                 "e":[0.0,100.0,0.0,1.0], "f":[100.0,100.0,0.0,1.0], "g":[100.0,100.0,100.0,1.0], "h":[0.0,100.0,100.0,1.0],
+                                 "i":[50.0,150.0,0.0,1.0], "j":[50.0,150.0,100.0,1.0]}
         self.pontos_modificados = None
 
-        #coloca os pontos originais dentro dos pontos modificados
+        # deslocar os pontos para o centro da tela
+        for ponto in self.pontos_originais:
+            self.pontos_originais[ponto][0] *= -1
+            self.pontos_originais[ponto][1] *= -1
+            self.pontos_originais[ponto][2] *= -1
+            self.pontos_originais[ponto][0] += 400
+            self.pontos_originais[ponto][1] += 400
+            self.pontos_originais[ponto][2] += 400
+
+        #coloca os pontos_modificados originais dentro dos pontos_modificados modificados
         self.resetar_pontos()
 
         #Grupos de Elementos
@@ -71,8 +79,8 @@ class UI(QWidget):
         self.group_rotacao = self.findChild(QGroupBox, "groupRotacao")
         self.btn_origem = self.findChild(QRadioButton, "btnRotacaoOrigem")
         self.btn_centro_objeto = self.findChild(QRadioButton, "btnRotacaoCentroObjeto")
-        self.eixo = self.findChild(QComboBox, "comoBoxEixo")
-        self.input_rotacao_graus = self.findChild(QLineEdit, "inputRotacaoGraus")
+        self.eixo_rotacao = self.findChild(QComboBox, "comboBoxEixo")
+        self.input_angulo_rotacao = self.findChild(QLineEdit, "inputRotacaoGraus")
         
         #Grupo do Shearing
         self.group_shearing = self.findChild(QGroupBox, "groupShearing")
@@ -97,53 +105,51 @@ class UI(QWidget):
         self.btn_executar.clicked.connect(self.executar)
         self.btn_resetar_pontos.clicked.connect(self.resetar_pontos)
 
-        #rederizando a primeira casinha
-        self.render()
 
+        self.render()
+        
     
     def resetar_pontos(self):
+        self.pontos_modificados = self.pontos_originais.copy()
         print("Pontos resetados!!\n")
-        self.pontos_modificados = deepcopy(self.pontos_originais)
         self.render()
         
         
     def executar(self):
+
+        centro_objeto = self.centro_objeto()
+
         # Realiza as operações
         for key in self.pontos_modificados.keys():
             if self.group_escala.isChecked():
                 self.pontos_modificados[key] = self.operacao_escala(self.pontos_modificados[key])
-
             if self.group_translacao.isChecked():
                 self.pontos_modificados[key] = self.operacao_translacao(self.pontos_modificados[key])
-
             if self.group_rotacao.isChecked():
-                self.pontos_modificados[key] = self.operacao_rotacao(self.pontos_modificados[key])
-
+                self.pontos_modificados[key] = self.operacao_rotacao(self.pontos_modificados[key], centro_objeto)
             if self.group_shearing.isChecked():
                 self.pontos_modificados[key] = self.operacao_shearing(self.pontos_modificados[key])
-
-
         self.render()
 
 
     def render(self):
         # Desenha a tela 
         self.canvas.fill(QColor(255,255,255))
-        self.painter.drawLine(-self.pontos_modificados["a"][0]+500, -self.pontos_modificados["a"][1]+500, -self.pontos_modificados["b"][0]+500, -self.pontos_modificados["b"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["a"][0]+500, -self.pontos_modificados["a"][1]+500, -self.pontos_modificados["d"][0]+500, -self.pontos_modificados["d"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["a"][0]+500, -self.pontos_modificados["a"][1]+500, -self.pontos_modificados["e"][0]+500, -self.pontos_modificados["e"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["b"][0]+500, -self.pontos_modificados["b"][1]+500, -self.pontos_modificados["c"][0]+500, -self.pontos_modificados["c"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["b"][0]+500, -self.pontos_modificados["b"][1]+500, -self.pontos_modificados["f"][0]+500, -self.pontos_modificados["f"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["c"][0]+500, -self.pontos_modificados["c"][1]+500, -self.pontos_modificados["d"][0]+500, -self.pontos_modificados["d"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["c"][0]+500, -self.pontos_modificados["c"][1]+500, -self.pontos_modificados["g"][0]+500, -self.pontos_modificados["g"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["d"][0]+500, -self.pontos_modificados["d"][1]+500, -self.pontos_modificados["h"][0]+500, -self.pontos_modificados["h"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["e"][0]+500, -self.pontos_modificados["e"][1]+500, -self.pontos_modificados["h"][0]+500, -self.pontos_modificados["h"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["e"][0]+500, -self.pontos_modificados["e"][1]+500, -self.pontos_modificados["i"][0]+500, -self.pontos_modificados["i"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["f"][0]+500, -self.pontos_modificados["f"][1]+500, -self.pontos_modificados["i"][0]+500, -self.pontos_modificados["i"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["f"][0]+500, -self.pontos_modificados["f"][1]+500, -self.pontos_modificados["g"][0]+500, -self.pontos_modificados["g"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["g"][0]+500, -self.pontos_modificados["g"][1]+500, -self.pontos_modificados["j"][0]+500, -self.pontos_modificados["j"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["h"][0]+500, -self.pontos_modificados["h"][1]+500, -self.pontos_modificados["j"][0]+500, -self.pontos_modificados["j"][1]+500)
-        self.painter.drawLine(-self.pontos_modificados["i"][0]+500, -self.pontos_modificados["i"][1]+500, -self.pontos_modificados["j"][0]+500, -self.pontos_modificados["j"][1]+500)
+        self.painter.drawLine(int(self.pontos_modificados["a"][0]), int(self.pontos_modificados["a"][1]), int(self.pontos_modificados["b"][0]), int(self.pontos_modificados["b"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["a"][0]), int(self.pontos_modificados["a"][1]), int(self.pontos_modificados["d"][0]), int(self.pontos_modificados["d"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["a"][0]), int(self.pontos_modificados["a"][1]), int(self.pontos_modificados["e"][0]), int(self.pontos_modificados["e"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["b"][0]), int(self.pontos_modificados["b"][1]), int(self.pontos_modificados["c"][0]), int(self.pontos_modificados["c"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["b"][0]), int(self.pontos_modificados["b"][1]), int(self.pontos_modificados["f"][0]), int(self.pontos_modificados["f"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["c"][0]), int(self.pontos_modificados["c"][1]), int(self.pontos_modificados["d"][0]), int(self.pontos_modificados["d"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["c"][0]), int(self.pontos_modificados["c"][1]), int(self.pontos_modificados["g"][0]), int(self.pontos_modificados["g"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["d"][0]), int(self.pontos_modificados["d"][1]), int(self.pontos_modificados["h"][0]), int(self.pontos_modificados["h"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["e"][0]), int(self.pontos_modificados["e"][1]), int(self.pontos_modificados["h"][0]), int(self.pontos_modificados["h"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["e"][0]), int(self.pontos_modificados["e"][1]), int(self.pontos_modificados["i"][0]), int(self.pontos_modificados["i"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["f"][0]), int(self.pontos_modificados["f"][1]), int(self.pontos_modificados["i"][0]), int(self.pontos_modificados["i"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["f"][0]), int(self.pontos_modificados["f"][1]), int(self.pontos_modificados["g"][0]), int(self.pontos_modificados["g"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["g"][0]), int(self.pontos_modificados["g"][1]), int(self.pontos_modificados["j"][0]), int(self.pontos_modificados["j"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["h"][0]), int(self.pontos_modificados["h"][1]), int(self.pontos_modificados["j"][0]), int(self.pontos_modificados["j"][1]))
+        self.painter.drawLine(int(self.pontos_modificados["i"][0]), int(self.pontos_modificados["i"][1]), int(self.pontos_modificados["j"][0]), int(self.pontos_modificados["j"][1]))
 
         self.label_img.setPixmap(self.canvas)
 
@@ -152,7 +158,7 @@ class UI(QWidget):
         # Matriz de multiplicação
         matriz = np.identity(4)
 
-        # Tranformando em nupy array para fazer a multiplicação
+        # Tranformando em numpy array para fazer a multiplicação
         ponto = np.array(ponto)
 
         # Se for escala Global
@@ -193,7 +199,7 @@ class UI(QWidget):
         # Matriz de multiplicação
         matriz = np.identity(4)
 
-        # Tranformando em nupy array para fazer a multiplicação
+        # Tranformando em numpy array para fazer a multiplicação
         ponto = np.array(ponto)
 
         # pegando os valores de entrada
@@ -209,28 +215,45 @@ class UI(QWidget):
         return produto
 
 
-    def operacao_rotacao(self, ponto: list) -> list:
-        # Matriz de multiplicação
-        matriz = np.identity(4)
-
-        # Tranformando em nupy array para fazer a multiplicação
+    def operacao_rotacao(self, ponto: list, centro_objeto: list) -> list:
+        # Tranformando em numpy array para fazer a multiplicação
         ponto = np.array(ponto)
 
         #se for rotação na origem
         if self.btn_origem.isChecked():
-            # rotação apenas com as matrizes de rotação
+            # rotação apenas com as matrizes de rotação sem deslocamento
+            matriz = self.matriz_rotacao()
             
+            # Fazendo o produto
+            produto = np.dot(ponto, matriz)
+            
+            #verificando as coordenadas homegêneas
+            produto = self.verifica_coord_homogenea(produto)
 
             return produto
 
         # se for rotação no centro do objeto
         elif self.btn_centro_objeto.isChecked():
-            # leva o obejto até a origem, rotaciona e volta para a aposição original
+            # leva o centro do objeto até a origem, rotaciona e volta para a aposição original
+            matriz = self.matriz_rotacao()
+            matriz_translacao_centro = np.identity(4)
+            matriz_translacao_centro[3][0] = -centro_objeto[0]
+            matriz_translacao_centro[3][1] = -centro_objeto[1]
+            matriz_translacao_centro[3][2] = -centro_objeto[2]
+            matriz_translacao_centro[3][3] = 1
+
+            matriz_translacao_centro_inversa = np.identity(4)
+            matriz_translacao_centro_inversa[3][0] = centro_objeto[0]
+            matriz_translacao_centro_inversa[3][1] = centro_objeto[1]
+            matriz_translacao_centro_inversa[3][2] = centro_objeto[2]
+            matriz_translacao_centro_inversa[3][3] = 1
+
+            produto = np.dot(np.dot(np.dot(ponto, matriz_translacao_centro), matriz), matriz_translacao_centro_inversa)
             
+            #verificando as coordenadas homegêneas
+            produto = self.verifica_coord_homogenea(produto)
 
-
-            print("Centro do Objeto\n")
-            return ponto
+            return produto
 
         # se nada for selecionado retornar o próprio ponto,
         # é um caso impossível, mas apenas por garantia
@@ -238,11 +261,11 @@ class UI(QWidget):
             return ponto
 
 
-    def operacao_shearing(self, ponto):
+    def operacao_shearing(self, ponto: list) -> list:
         # Matriz de multiplicação
         matriz = np.identity(4)
 
-        # Tranformando em nupy array para fazer a multiplicação
+        # Tranformando em numpy array para fazer a multiplicação
         ponto = np.array(ponto)
 
         # Pegando os valores de entrada
@@ -272,13 +295,53 @@ class UI(QWidget):
         return produto
 
 
-    def verifica_coord_homogenea(self, produto):
+    def verifica_coord_homogenea(self, produto: list) -> list:
         # Transformando em coordenadas homogênas ou somente fazendo o cast para inteiro
         if produto[3] != 1:
-            produto = [int(i/produto[3]) for i in produto]
+            produto = [i/produto[3] for i in produto]
         else:
-            produto = [int(i) for i in produto]
+            produto = [i for i in produto]
         return produto
+
+
+    def matriz_rotacao(self):
+        # retorna a matriz de rotação específica do tipo de rotação
+        eixo = str(self.eixo_rotacao.currentText())
+        angulo = float(self.input_angulo_rotacao.text())
+        
+        # verificando qual é o eixo de rotação
+        if eixo == "X":
+            return np.array([[1, 0, 0, 0],
+                            [0, np.cos(angulo), -np.sin(angulo), 0],
+                            [0, np.sin(angulo), np.cos(angulo), 0],
+                            [0, 0, 0, 1]])
+
+        elif eixo == "Y":
+            return np.array([[np.cos(angulo), 0, np.sin(angulo), 0],
+                            [0, 1, 0, 0],
+                            [-np.sin(angulo), 0, np.cos(angulo), 0],
+                            [0, 0, 0, 1]])
+            
+        #o exio Z será o padrão
+        else:
+            return np.array([[np.cos(angulo), -np.sin(angulo), 0, 0],
+                             [np.sin(angulo), np.cos(angulo), 0, 0],
+                             [0, 0, 1, 0],
+                             [0, 0, 0, 1]])
+    
+
+    def centro_objeto(self):
+        # retorna o centro do objeto passabdo pelos pontos modificados
+        tamanho = len(self.pontos_modificados.values())
+        x = 0
+        y = 0
+        z = 0
+
+        for i in self.pontos_modificados.values():
+            x += i[0]
+            y += i[1]
+            z += i[2]
+        return [x/tamanho, y/tamanho, z/tamanho]
 
 
 if __name__ == '__main__':
